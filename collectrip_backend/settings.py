f913c279680e25c3efae56ddbfe669e6e1b54dee
jinyoung0711 <ssconvergence2@gmail.com>
2025-08-04 18:47:59 +0900

    🎨 구조개선 및 초기 설정

--- collectrip_backend/settings.py
+++ collectrip_backend/settings.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 """
 Django settings for collectrip_backend project.
 
@@ -37,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.users',
+    'apps.collector',
+    'apps.contents',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +84,15 @@ WSGI_APPLICATION = 'collectrip_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
